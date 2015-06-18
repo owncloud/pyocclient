2f1bd246fa9b01d0432f15dfb8cacc9d2dd6adc6
SergioBertolinSG <sbertolin@solidgear.es>
2015-06-18 12:47:02 +0200

    taking into account situations without provisioning api

--- owncloud/test/test.py
+++ owncloud/test/test.py
@@ -39,13 +39,18 @@ class TestFileAccess(unittest.TestCase):
             self.test_root = '/' + self.test_root
         self.client.mkdir(self.test_root)
         self.share2user = Config['owncloud_share2user']
-        self.client.create_user(self.share2user, 'share')
-
+	try:
+	    self.client.create_user(self.share2user, 'share')
+        except:
+	    pass
 
     def tearDown(self):
         self.client.delete(self.test_root)
-	self.client.delete_user(self.share2user)
-        self.client.logout()
+	try:
+	    self.client.delete_user(self.share2user)
+	except:
+            pass
+	self.client.logout()
         shutil.rmtree(self.temp_dir)
 
     @staticmethod
