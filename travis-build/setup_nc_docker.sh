a3137688bbfcef88959e35e775b23196bfbb16d1
Arthur Schiwon <blizzz@arthur-schiwon.de>
2017-07-24 11:51:00 +0200

    test against Nc

--- travis-build/setup_nc_docker.sh
+++ travis-build/setup_nc_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 OC_VERSION=$1
 
-docker pull owncloud:$OC_VERSION
-DOCKER_ID=$(docker run -d -p 80:80 owncloud:$OC_VERSION)
+docker pull nextcloud:$OC_VERSION
+DOCKER_ID=$(docker run -d -p 80:80 nextcloud:$NC_VERSION)
 
 # needed else occ isn't available directly...
 sleep 5
